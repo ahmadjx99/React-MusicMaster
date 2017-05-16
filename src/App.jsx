2225de2aeea8ed61568add703a65380080e5b2db
Haris Sucipto <harisssucipto@gmail.com>
2017-05-16 10:01:17 +0700

    add url to fecth

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,19 @@ import './App.css';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+  search() {
+    console.log('this.state', this.state);
+    const BASE_URL = 'https://api.spotify.com/v1/search';
+    const FETCH_URL = `${BASE_URL}?q=${this.state.query}&type=artist&limit=1`;
+    console.log('FETCH_URL', FETCH_URL);
+  }
 
   render() {
     return (
@@ -13,8 +26,11 @@ class App extends Component {
             <FormControl 
               type="text" 
               placeholder="Search for an Artist"
+              value={this.state.query}
+              onChange={(event) => this.setState({query: event.target.value})}
+              onKeyPress={(event) => event.key === 'Enter' ? this.search() : ''}
             />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
